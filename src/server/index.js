cf04ae36ccf3344e406000d6dfa4162478e3dd26
Rem Lampa <rem@remlampa.com>
2018-11-10 23:09:11 +0800

    Init /prices api endpoint :sparkles:

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const os = require('os');
 const path = require('path');
 
+const db = require('./database');
+
+const data = db.initDatabase();
+
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/prices/rice', (req, res) => {
+  res.send(data.ricePrices);
+});
+
+app.get('/api/prices/wheat', (req, res) => {
+  res.send(data.wheatPrices);
+});
+
+app.get('/api/prices/maize', (req, res) => {
+  res.send(data.maizePrices);
+});
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
