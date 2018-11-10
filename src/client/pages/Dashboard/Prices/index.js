e06e6d1844bbe234f3a6dd82ec738f09c632bee4
Marbille Juntado <marbille.juntado@hotmail.ph>
2018-11-11 00:08:46 +0800

    Add prices table

---
+++ src/client/pages/Dashboard/Prices/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Prices = () => (
+	<div className="o-box-container prices-wrapper">
+		<table className="table">
+			<thead>
+				<tr>
+					<th>Product</th>
+					<th>Ask</th>
+					<th>Bid</th>
+					<th>Low</th>
+					<th>High</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<strong>RICE</strong>
+					</td>
+					<td>
+						<div className="high">143.017</div>
+					</td>
+					<td>
+						<div className="high">143.035</div>
+					</td>
+					<td>142.396</td>
+					<td>144.018</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Maize</strong>
+					</td>
+					<td>
+						<div className="low">285.765</div>
+					</td>
+					<td>
+						<div className="low">285.982</div>
+					</td>
+					<td>284.002</td>
+					<td>286.201</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Wheat</strong>
+					</td>
+					<td>
+						<div className="low">400.574</div>
+					</td>
+					<td>
+						<div className="same">400.723</div>
+					</td>
+					<td>400.390</td>
+					<td>400.883</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+);
+
+export default Prices;
